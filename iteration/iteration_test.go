d7c0cd0249a73fec49b9126377530f893746c23e
Charles Parent <cha.parent@gmail.com>
2022-05-24 09:21:55 +0200

    Add iteration: for, Benchmark
    Signed-off-by: Charles Parent <cha.parent@gmail.com>

---
+++ iteration/iteration_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 4)
+	expected := "aaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+// Benchmarking
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 4)
+	}
+}
+
+func ExampleRepeat() {
+	repeat := Repeat("&", 5)
+	fmt.Println(repeat)
+	// Output: &&&&&
+}
