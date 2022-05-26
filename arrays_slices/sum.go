7ec9af5f6299af7e933df9440b9a850768d2d9c1
Charles Parent <cha.parent@gmail.com>
2022-05-27 00:01:08 +0200

    slices_2
    Signed-off-by: Charles Parent <cha.parent@gmail.com>

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -7,3 +7,31 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	/*
+		lengthOfNumbers := len(numbersToSum)
+		sums := make([]int, lengthOfNumbers)
+	*/
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	/*
+		lengthOfNumbers := len(numbersToSum)
+		sums := make([]int, lengthOfNumbers)
+	*/
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return
+}
